53ea403c5a219aee72ca3574a71c98c5b46a06e4
Mauve Signweaver <contact@mauve.moe>
2025-12-27 00:07:06 -0500

    docs: Add full corestore types

--- types/corestore.d.ts
+++ types/corestore.d.ts
@@ -1,16 +1,29 @@
 declare module "corestore" {
   import type { Connection } from "hyperswarm";
   import type { CoreOpts } from "hypercore";
-  import type Hypercore from "hypercore";
+  import type Hypercore, { Key, KeyPair } from "hypercore";
   import type RocksDB from "rocksdb-native";
-  interface CoreStoreOpts {}
+  import { Readable } from "streamx";
+  interface CoreStoreOpts {
+    writable?: boolean;
+    readOnly: boolean;
+    primaryKey?: Key;
+    unsafe?: boolean;
+  }
+  type GetOpts = CoreOpts | { name: string } | { key: Key };
   export default class CoreStore {
-    constructor(storage: strin | RocksDB, opts: CoreStoreOpts?);
+    constructor(storage: string | RocksDB, opts: CoreStoreOpts?);
+    get<DataType>(opts: GetOpts): Hypercore<DataType>;
+    ready(): Promise<void>;
+    close(): Promise<void>;
     namespace(namespace: string): CoreStore;
-    get<DataType>(opts: CoreOpts): Hypercore<DataType>;
-    ready(): Promise<void>
-    close(): Promise<void>
-    createKeyPair(name: string): KeyPair
-    replicate(connection: Connection)
+    session(): CoreStore;
+    list(namespace?: string): Readable<Key>;
+    watch(cb: (core: Hypercore) => void);
+    unwatch(cb: (core: Hypercore) => void);
+    suspend(): Promise<void>;
+    resume(): Promise<void>;
+    createKeyPair(name: string): KeyPair;
+    replicate(connection: Connection);
   }
 }
