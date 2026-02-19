eec6d9c05f901dbf5a5ea6a630fcdef85a1df8d7
Mauve Signweaver <contact@mauve.moe>
2026-02-19 13:27:08 -0500

    chore: Add types for RocksDB iterator and session API

--- types/rocksdb-native.d.ts
+++ types/rocksdb-native.d.ts
@@ -1,14 +1,47 @@
 declare module "rocksdb-native" {
+  import type { Encoder } from "compact-encoding";
+
+  interface SessionOpts {
+    valueEncoding?: Encoder;
+    keyEncoding?: Encoder;
+    columnFamily?: string;
+    readOnly?: boolean;
+  }
+
+  interface IteratorOpts<Key> {
+    gt?: Key;
+    gte?: Key;
+    lt?: Key;
+    lte?: Key;
+    reverse?: boolean;
+    limit?: number;
+    values?: boolean;
+    keys?: boolean;
+  }
+
+  interface Entry<Key, Value> {
+    key: Key;
+    value: Value;
+  }
+
   /**
    * RocksDB-Native Instance.
    * **WARNING**: This is a stub used in hyper-sdk.
    * Most methods are not yet documented
    */
-  export default class RocksDB {
-    constructor(storageLocation: string);
-    put(key: string, value: string): Promise<void>;
-    get(key: string): string?;
+  export default class RocksDB<Key = string, Value = string> {
+    constructor(storageLocation: string, options?: SessionOpts);
+    put(key: Key, value: Value): Promise<void>;
+    get(key: Key): Promise<Value?>;
+    delete(key: Key): Promise<void>;
     flush(): Promise<void>;
     close(): Promise<void>;
+    session<SubKey = Key, SubValue = Value>(
+      options?: SessionOpts
+    ): RocksDB<SubKey, SubValue>;
+    iterator(
+      range?: IteratorOpts,
+      options?: IteratorOpts
+    ): AsyncIterable<Entry<Key, Value>>;
   }
 }
